38f759e6bd60d43a5e88ccfb7701bfd64a53f7f9
Tydarius Young <ty3226@rit.edu>
2019-03-01 17:40:43 -0500

    +Created Player class

--- src/main/java/com/webcheckers/model/Player.java
+++ src/main/java/com/webcheckers/model/Player.java
@@ -1,5 +1,47 @@
 package com.webcheckers.model;
 
 public class Player {
-//hi
+    public enum PlayerColor{
+        RED,
+        WHITE,
+        NOT_IN_GAME
+    }
+
+    private String username;
+
+    private PlayerColor playerColor;
+
+    public Player(String username){
+        this.username = username;
+        playerColor = PlayerColor.NOT_IN_GAME;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPlayerColor(boolean isPlayerOne) {
+        if(isPlayerOne)
+            playerColor = PlayerColor.RED;
+        else
+            playerColor = PlayerColor.WHITE;
+    }
+
+    public PlayerColor getPlayerColor() {
+        return playerColor;
+    }
+
+    public boolean equals(Object obj) {
+        if(obj == this) return true;
+        if(!(obj instanceof Player)) return false;
+
+        final Player that = (Player) obj;
+
+        return this.username.equals(that.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return username.hashCode();
+    }
 }
