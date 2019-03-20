7b50594622c7e340efabf1d0832ed651649f5839
jwy4877 <jwy4877@g.rit.edu>
2019-03-19 21:01:27 -0400

    Created PlayerTester and GetGameRouteTester java classes to test the two classes

---
+++ src/test/java/com/webcheckers/model/PlayerTester.java
@@ -0,0 +1,41 @@
+package com.webcheckers.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("Model-tier")
+public class PlayerTester {
+
+    @Test
+    public void testPlayerCreation() {
+        final Player CuT = new Player("Test Player");
+        assertNotNull(CuT, "Player is not null");
+    }
+
+    @Test
+    public void testPlayerName() {
+        final Player CuT = new Player("Test Player");
+        assertEquals("Test Player", CuT.getName(), "Player name set correctly");
+    }
+
+    @Test
+    public void testPlayerNoColor() {
+        final Player CuT = new Player("Test Player");
+        assertEquals(Player.PlayerColor.NOT_IN_GAME, CuT.getPlayerColor());
+    }
+
+    @Test
+    public void testPlayerOneColor() {
+        final Player CuT = new Player("Test Player");
+        CuT.setPlayerColor(true);
+        assertEquals(Player.PlayerColor.RED, CuT.getPlayerColor());
+    }
+
+    @Test
+    public void testPlayerTwoColor() {
+        final Player CuT = new Player("Test Player");
+        CuT.setPlayerColor(false);
+        assertEquals(Player.PlayerColor.WHITE, CuT.getPlayerColor());
+    }
+}
---
+++ src/test/java/com/webcheckers/ui/GetGameRouteTester.java
@@ -0,0 +1,10 @@
+package com.webcheckers.ui;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class GetGameRouteTester {
+
+
+}
