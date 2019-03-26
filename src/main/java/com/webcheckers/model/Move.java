4d5c2395f05cd3ab7ef5be38dca97502918cc6f5
jwy4877 <jwy4877@g.rit.edu>
2019-03-25 22:22:02 -0400

    added to Move and created Position

--- src/main/java/com/webcheckers/model/Move.java
+++ src/main/java/com/webcheckers/model/Move.java
@@ -1,17 +1,41 @@
 package com.webcheckers.model;
 
+import javafx.geometry.Pos;
+
+import java.text.ParsePosition;
+
 public class Move {
+
+    private Position start;
+    private Position end;
 
     public Move(){}
 
-    public void movePiece( Space src, Space dst ) {
-        if( validMove( src, dst ) ) {
-            dst.setPiece(src.getPiece());
-            src.setPiece(null);
+    /**
+     * The starting position of the move
+     * @return the position
+     */
+    public Position getStart() {
+        return this.start;
+    }
+
+    /**
+     * The ending position of the move
+     * @return the position
+     */
+    public Position getEnd() {
+        return this.end;
+    }
+
+    public void movePiece( Position startingPos, Position endingPos ) {
+        if( validMove( startingPos, endingPos ) ) {
+
         }
     }
 
-    private boolean validMove( Space src, Space dst ) {
+    private boolean validMove( Position startingPos, Position endingPos ) {
         return true;
     }
+
+
 }
---
+++ src/main/java/com/webcheckers/model/Position.java
@@ -0,0 +1,29 @@
+package com.webcheckers.model;
+
+public class Position {
+
+    private int row;  // { 0 to 7 }
+    private int cell;  // { 0 to 7 }
+
+    public Position( int row, int cell ) {
+        this.row = row;
+        this.cell = cell;
+    }
+
+    /**
+     * The row index of this position
+     * @return an int from 0 to 7
+     */
+    public int getRow() {
+        return this.row;
+    }
+
+
+    /**
+     * The cell (column) index of this position
+     * @return an int from 0 to 7
+     */
+    public int getCell() {
+        return this.cell;
+    }
+}
