6fc33cced02821b53cf08ca2e60ae78d85635891
cxl8351 <cxl8351@rit.edu>
2019-04-01 00:36:30 -0400

    Save Point

---
+++ src/test/java/com/webcheckers/model/PieceTest.java
@@ -0,0 +1,21 @@
+package com.webcheckers.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PieceTest {
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void getColor() {
+    }
+
+    @Test
+    void equals() {
+    }
+
+}
---
+++ src/test/java/com/webcheckers/model/PositionTest.java
@@ -0,0 +1,28 @@
+package com.webcheckers.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PositionTest {
+
+    @Test
+    void getRow() {
+    }
+
+    @Test
+    void getCell() {
+    }
+
+    @Test
+    void compare() {
+    }
+
+    @Test
+    void difference() {
+    }
+
+    @Test
+    void equals() {
+    }
+}
