e41b069518de2bb41172c0b9d930a0b21d051841
jwy4877 <jwy4877@g.rit.edu>
2019-04-01 04:06:43 -0400

    GameCenterTest and PlayerLobbyTest working

--- src/test/java/com/webcheckers/ApplicationTest.java
+++ src/test/java/com/webcheckers/ApplicationTest.java
@@ -3,8 +3,11 @@ package com.webcheckers;
 import org.junit.jupiter.api.Test;
 
 public class ApplicationTest {
+
     @Test
     public void testRunApplication(){
         Application.main(null);
     }
+
+
 }
