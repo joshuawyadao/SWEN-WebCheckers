0578d01456e139ddc3ca5ad5605540637973e80d
jwy4877 <jwy4877@g.rit.edu>
2019-04-13 18:42:56 -0400

    Worked on next and previous replay game

--- src/main/java/com/webcheckers/ui/Replay/PostReplayPreviousTurnRoute.java
+++ src/main/java/com/webcheckers/ui/Replay/PostReplayPreviousTurnRoute.java
@@ -3,7 +3,9 @@ package com.webcheckers.ui.Replay;
 import com.google.gson.Gson;
 import com.webcheckers.appl.GameCenter;
 import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.model.Board;
 import com.webcheckers.model.Player;
+import com.webcheckers.model.ReplayGame;
 import com.webcheckers.ui.Home.GetHomeRoute;
 import com.webcheckers.ui.PlayGame.GetGameRoute;
 import com.webcheckers.util.Message;
@@ -34,19 +36,20 @@ public class PostReplayPreviousTurnRoute implements Route {
     public Object handle(Request request, Response response) {
         LOG.finer("PostReplayPreviousTurnRoute is invoked.");
         Map<String, Object> vm = new HashMap<>();
-
         Session currentSession = request.session();
+
         Player currentUser = currentSession.attribute(GetHomeRoute.CURRENT_USER_ATTR);
+        String gameId = currentSession.attribute(GetReplayGameRoute.GAME_ID_ATTR);
 
-        String gameId = currentSession.attribute(GetGameRoute.GAME_ID_ATTR);
+        //TODO: MOVE THE GAME TO THE PREVIOUS STATE
 
+        ReplayGame replayGame = this.gameCenter.getReplayGame(gameId);
+        Board nextTurn = replayGame.getPreviousTurn();
 
-        //   MOVE THE GAME TO THE PREVIOUS STATE
-
-
+        // If clicking next button was succesful
         Message successInfo;
 
-        if( 1 > 0 ){
+        if( nextTurn != null ){
             successInfo = Message.info("true");
         }else{
             successInfo = Message.info("false");
