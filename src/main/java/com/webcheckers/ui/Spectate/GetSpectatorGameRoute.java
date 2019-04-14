2968941d84ca6d3b46255a6c721699040a04366b
rml1168 <rml1168@rit.edu>
2019-04-14 16:13:46 -0400

    GetSpectatorGameRoute +added proper vm attributes

--- src/main/java/com/webcheckers/ui/Spectate/GetSpectatorGameRoute.java
+++ src/main/java/com/webcheckers/ui/Spectate/GetSpectatorGameRoute.java
@@ -5,6 +5,7 @@ import com.webcheckers.appl.GameCenter;
 import com.webcheckers.appl.PlayerLobby;
 import com.webcheckers.model.Game;
 import com.webcheckers.model.Player;
+import com.webcheckers.ui.BoardView;
 import com.webcheckers.ui.Home.GetHomeRoute;
 import spark.*;
 
@@ -39,18 +40,20 @@ public class GetSpectatorGameRoute implements Route {
     public Object handle(Request request, Response response) {
         LOG.finer("GetSpectatorGameRoute is invoked.");
         final Map<String, Object> vm = new HashMap<>();
-        Map<String, Object> modeOptions = new HashMap<>();
         Session currentSession = request.session();
         Player currentUser = currentSession.attribute(GetHomeRoute.CURRENT_USER_ATTR);
 
         String gameID = request.queryParams("gameID");
 
         Game gameToSpec = gameCenter.getGame(gameID);
-        vm.put("currentUser", currentUser);
+
+        vm.put(GetHomeRoute.CURRENT_USER_ATTR, currentUser);
         vm.put("viewMode", GameCenter.ViewMode.SPECTATOR);
         vm.put("redPlayer", gameToSpec.getRedPlayer());
         vm.put("whitePlayer", gameToSpec.getWhitePlayer());
         vm.put("activeColor", gameToSpec.getActivePlayer());
+        vm.put("board", new BoardView(gameToSpec.getCheckerBoard(),
+                gameToSpec.getActivePlayer() == gameToSpec.getRedPlayer()));
 
         return templateEngine.render(new ModelAndView(vm, VIEW_NAME));
     }
